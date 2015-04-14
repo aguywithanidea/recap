277622f64c647d5180b3dc7f9624459857cf9505
Richard Luck <richard@loudlever.com>
2015-04-14 15:02:12 -0700

    introducing devise templates for modification

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410174140) do
+ActiveRecord::Schema.define(version: 20150414212514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150410174140) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "remember_created_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
